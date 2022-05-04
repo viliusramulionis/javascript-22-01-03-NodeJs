37ec099f674716adb967661d98c0b006e3400cd4
viliusramulionis <35171606+viliusramulionis@users.noreply.github.com>
2022-05-04 11:24:26 +0300

    Delete update

--- paskaitos/2022-05-04/index.js
+++ paskaitos/2022-05-04/index.js
@@ -168,5 +168,35 @@ app.post('/client-submit', async (req, res) => {
   }
 })
 
+//Kliento istrynimas delete metodu
+
+app.delete('/delete-client/:id', async (req, res) => {
+  const id = req.params.id 
+
+  if(!id) {
+    res.json({status: 'failed', message: 'Neperduotas joks id'})
+    return 
+  }
+
+  try {
+
+    const data = await fs.readFile('./database.json', 'utf8')
+
+    let parsedJson = JSON.parse(data)
+
+    parsedJson.splice(id, 1)
+
+    //Informacijos issaugojimas faile
+
+    await fs.writeFile('./database.json', JSON.stringify(parsedJson))
+
+    res.json({status: 'ok', message: 'Įrašas sėkmingai ištrintas'})
+
+  } catch {
+      res.json({status: 'failed', message: 'Nepavyko perskaityti duomenu bazes failo'})
+  }
+
+})
+
 //Sukuria serveri priskiriant jam routerius
 app.listen(port)
